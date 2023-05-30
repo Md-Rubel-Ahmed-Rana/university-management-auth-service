41012b5d8fa7d76e50394705348a56d46847b92e
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2023-05-31 00:02:58 +0600

    Code editor setup completed

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import express, { Application, Request, Response } from "express"
-import cors from "cors"
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 
-const app : Application = express();
+const app: Application = express()
 
 app.use(cors())
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // base route
-app.get("/", (req: Request, res: Response) => {
-    res.send("University server is working fine!")
+app.get('/', (req: Request, res: Response) => {
+  res.send('University server is working fine!')
 })
 export default app
