bd6eca9c072a3b82c15aa528c6ce586685aeed50
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2023-05-30 21:44:41 +0600

    basic setup completed

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "../config/index"
+
+
+async function bootstrap() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log("Database connected");
+    app.listen(config.port, () => {
+      console.log(`Server is running on port ${config.port}`);
+    })
+  } catch (error) {
+    console.log("Database not connected");
+  }
+}
+
+bootstrap()
