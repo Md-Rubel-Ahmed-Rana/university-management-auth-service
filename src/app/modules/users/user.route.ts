32b3c4ed54f9988613be0bd576e65a5432f76a36
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2023-06-21 01:15:42 +0600

    Zod validator isntalled and basic setup

--- src/app/modules/users/user.route.ts
+++ src/app/modules/users/user.route.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express'
+import validateRequest from '../../middlewares/validateRequest'
 import { UserController } from './user.controllers'
+import { UserValidation } from './user.validation'
 
 const userRouter = Router()
 
-userRouter.post('/create-user', UserController.createUser)
+userRouter.post(
+  '/create-user',
+  validateRequest(UserValidation.createUserZodSchema),
+  UserController.createUser
+)
 
 export const UserRoutes = userRouter
